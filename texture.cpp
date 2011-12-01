e05483827d3e085ff38d96f2629bf6776062cc50
Jesse <jcantara@gmail.com>
2011-11-30 21:11:07 -0500

    Texture scaling methods need to be specified

--- texture.cpp
+++ texture.cpp
@@ -60,8 +60,8 @@ cTexture::cTexture(string Filename) {
   glBindTexture(GL_TEXTURE_2D, texture);
   
   // not sure if these are necessary yet, defaults may be fine
-  //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   // move our sdl surface into opengl memory
   glTexImage2D(GL_TEXTURE_2D, 0, numcolors, width, height, 0, texture_format, GL_UNSIGNED_BYTE, surface->pixels);
