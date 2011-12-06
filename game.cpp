c0ef5552130d9d20114118b6061806b541af6c42
Jesse <jcantara@gmail.com>
2011-12-05 22:14:24 -0500

    remove debug, make rendering faster

--- game.cpp
+++ game.cpp
@@ -17,6 +17,7 @@ cGame::cGame() : m_Fps(50), m_Eps(50), m_TextureManager() {
   // make sure SDL cleans up before exit
   atexit(SDL_Quit);
 
+  SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 0 );
   // create a new window
   const SDL_VideoInfo* info = SDL_GetVideoInfo();
   m_pScreen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, info->vfmt->BitsPerPixel, SDL_OPENGL);
@@ -104,7 +105,7 @@ void cGame::Physicsloop() {
     loops++;
     m_Eps.event();
     Physics(); // call this in child class
-    //cout << "P: " << m_Eps.persecond() << endl;
+    cout << "P: " << m_Eps.persecond() << endl;
   }
   if (loops == MAX_FRAMESKIP) {
     // computer is too slow to run game
@@ -117,24 +118,25 @@ void cGame::Drawingloop() {
   m_Fps.event();
 
   //SDL_FillRect(m_pScreen, 0, SDL_MapRGB(m_pScreen->format, 0, 0, 0)); // black background
-  cout << "  glClear: " << SDL_GetTicks() << endl;
+  //cout << "  glClear: " << SDL_GetTicks() << endl;
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // Clear The Screen And The Depth Buffer
-  cout << "  glClear Done: "  << SDL_GetTicks() << endl;
-  cout << "  glLoadIdentity: " << SDL_GetTicks() << endl;
+  //cout << "  glClear Done: "  << SDL_GetTicks() << endl;
+  //cout << "  glLoadIdentity: " << SDL_GetTicks() << endl;
   glLoadIdentity();
-  cout << "  glLoadIdentity Done: " << SDL_GetTicks() << endl;
+  //cout << "  glLoadIdentity Done: " << SDL_GetTicks() << endl;
 
   float interpolation = float( SDL_GetTicks() + SKIP_TICKS - m_iNextGameTick ) / float( SKIP_TICKS );
-  cout << "  myGame.Draw: " << SDL_GetTicks() << endl;
+  //cout << "  myGame.Draw: " << SDL_GetTicks() << endl;
   Draw(interpolation); // call this in child class
-  cout << "  myGame.Draw Done: " << SDL_GetTicks() << endl;
+  //cout << "  myGame.Draw Done: " << SDL_GetTicks() << endl;
   //cout << "D: " << m_Fps.persecond() << endl;
 
   //SDL_Flip(m_pScreen);
-  cout << "  SDL_GL_SwapBuffers: " << SDL_GetTicks() << endl;
+  //cout << "  SDL_GL_SwapBuffers: " << SDL_GetTicks() << endl;
+  //glFinish();
   //SDL_GL_SwapBuffers();
-  glFinish();
-  cout << "  SDL_GL_SwapBuffers Done: " << SDL_GetTicks() << endl;
+  //glFinish();
+  //cout << "  SDL_GL_SwapBuffers Done: " << SDL_GetTicks() << endl;
 }
 
 int cGame::Go() {
@@ -143,14 +145,14 @@ int cGame::Go() {
   m_iNextGameTick = SDL_GetTicks();
   m_bGameIsRunning = true;
   while( m_bGameIsRunning ) {
-    cout << "Start Game Loop: " << endl;
-    cout << "Game Physics Loop: " << SDL_GetTicks() << endl;
+    //cout << "Start Game Loop: " << endl;
+    //cout << "Game Physics Loop: " << SDL_GetTicks() << endl;
     Physicsloop();
-    cout << "Game Physics Loop Done: " << SDL_GetTicks() << endl;
-    cout << "Game Drawing Loop: " << SDL_GetTicks() << endl;
+    //cout << "Game Physics Loop Done: " << SDL_GetTicks() << endl;
+    //cout << "Game Drawing Loop: " << SDL_GetTicks() << endl;
     Drawingloop();
-    cout << "Game Drawing Loop Done: " << SDL_GetTicks() << endl << endl;
-    cout << "ERRORS: " << glGetError() << endl;
+    //cout << "Game Drawing Loop Done: " << SDL_GetTicks() << endl << endl;
+    //cout << "ERRORS: " << glGetError() << endl;
   }
   End(); // child class cleanup
   return 0;
