f2f619d98900c12e64ba48f6802ad2b0e7718da5
Jesse <jcantara@gmail.com>
2011-12-12 19:18:40 -0500

    just playing with some config

--- game.cpp
+++ game.cpp
@@ -17,24 +17,24 @@ cGame::cGame() : m_Fps(50), m_Eps(50), m_TextureManager() {
   // make sure SDL cleans up before exit
   atexit(SDL_Quit);
 
-  SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 0 );
+  SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 0);
+  SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
+  SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
+  SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
+  SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
+  SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
   // create a new window
-  const SDL_VideoInfo* info = SDL_GetVideoInfo();
-  m_pScreen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, info->vfmt->BitsPerPixel, SDL_OPENGL);
+  //const SDL_VideoInfo* info = SDL_GetVideoInfo();
+  m_pScreen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 0, SDL_OPENGL | SDL_FULLSCREEN);
   if ( !m_pScreen ) {
     cout << "Unable to set video: " << SDL_GetError() << endl;
     exit(1);
   }
 
-  //m_Fps = new cEps(50);
-  //m_Eps = new cEps(50);
-
-  //m_TextureManager = new cTextureManager();
-
   // opengl init:
   glEnable(GL_TEXTURE_2D);
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);   // This Will Clear The Background Color To Black
-  //glClearDepth(1.0);        // Enables Clearing Of The Depth Buffer
+  glClearDepth(1.0);        // Enables Clearing Of The Depth Buffer
   glDepthFunc(GL_LESS);       // The Type Of Depth Test To Do
   glDisable(GL_DEPTH_TEST); // disable depth test to enable blend below
   glEnable(GL_BLEND);
@@ -105,13 +105,14 @@ void cGame::Physicsloop() {
     loops++;
     m_Eps.event();
     Physics(); // call this in child class
-    //cout << "P: " << m_Eps.persecond() << endl;
+    cout << "P: " << m_Eps.persecond() << endl;
   }
   if (loops == MAX_FRAMESKIP) {
     // computer is too slow to run game
     std::cout << "computer too slow" << std::endl;
     //return 1;
   }
+  //m_iNextGameTick += SKIP_TICKS;
 }
 
 void cGame::Drawingloop() {
@@ -122,31 +123,35 @@ void cGame::Drawingloop() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // Clear The Screen And The Depth Buffer
   //cout << "  glClear Done: "  << SDL_GetTicks() << endl;
   //cout << "  glLoadIdentity: " << SDL_GetTicks() << endl;
-  glLoadIdentity();
+  //glLoadIdentity();
   //cout << "  glLoadIdentity Done: " << SDL_GetTicks() << endl;
 
-  float interpolation = float( SDL_GetTicks() + SKIP_TICKS - m_iNextGameTick ) / float( SKIP_TICKS );
+  //float interpolation = float( SDL_GetTicks() + SKIP_TICKS - m_iNextGameTick ) / float( SKIP_TICKS );
   //cout << "  myGame.Draw: " << SDL_GetTicks() << endl;
-  Draw(interpolation); // call this in child class
+  //Draw(interpolation); // call this in child class
   //cout << "  myGame.Draw Done: " << SDL_GetTicks() << endl;
-  //cout << "D: " << m_Fps.persecond() << endl;
 
   //SDL_Flip(m_pScreen);
   //cout << "  SDL_GL_SwapBuffers: " << SDL_GetTicks() << endl;
   //glFinish();
-  //SDL_GL_SwapBuffers();
+  //glFlush();
+  SDL_GL_SwapBuffers();
   //glFinish();
   //cout << "  SDL_GL_SwapBuffers Done: " << SDL_GetTicks() << endl;
-  glFlush();
+  //glFlush();
+  cout << "D: " << m_Fps.persecond() << endl;
 }
 
 int cGame::Go() {
+  cout << "Starting Init" << endl;
   Init(); // child class setup
+  cout << "Init Done" << endl;
 
   m_iNextGameTick = SDL_GetTicks();
   m_bGameIsRunning = true;
   while( m_bGameIsRunning ) {
     //cout << "Start Game Loop: " << endl;
+    //cout << "Next Game Tick: " << m_iNextGameTick << endl;
     //cout << "Game Physics Loop: " << SDL_GetTicks() << endl;
     Physicsloop();
     //cout << "Game Physics Loop Done: " << SDL_GetTicks() << endl;
--- game.h
+++ game.h
@@ -17,11 +17,11 @@ class cGame
     cGame();
     ~cGame();
     int Go();
-    static const int TICKS_PER_SECOND = 100; // physics events per second
+    static const int TICKS_PER_SECOND = 50; // physics events per second
     static const int SKIP_TICKS = 1000 / TICKS_PER_SECOND; // calculate inverse of physics tick rate
     static const int MAX_FRAMESKIP = 5; // maximum allowed physics ticks without drawing a frame
-    static const int WINDOW_WIDTH = 800;
-    static const int WINDOW_HEIGHT = 400;
+    static const int WINDOW_WIDTH = 1024;
+    static const int WINDOW_HEIGHT = 600;
     // interface functions:
     virtual void Draw(const float interpolation) =0;
     virtual void Physics() =0;
